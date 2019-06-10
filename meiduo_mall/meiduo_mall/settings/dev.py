5fff997466db7c57d4d6ca8abcf2fbe1663c4514
杨超 <qingchengmuyu@gmail.com>
2019-06-10 16:56:38 +0800

    创建静态文件

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -149,6 +149,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
 
 
 CACHES = {
@@ -211,3 +214,5 @@ LOGGING = {
         },
     }
 }
+
+
