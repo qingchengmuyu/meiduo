03aec77a6969338afdf7ed2aacccdcd11c9a0b35
杨超 <qingchengmuyu@gmail.com>
2019-06-11 16:23:15 +0800

    追加导包路径，展示用户注册页面

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -10,10 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
-import os
+import os, sys
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# print(BASE_DIR)
+sys.path.insert(0, os.path.join(BASE_DIR, "apps"))
+# print("*" * 50)
+# print(sys.path)
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'meiduo_mall.apps.users',
+    'users'
 ]
 
 MIDDLEWARE = [
