f5fc57b3784143eed26d979b834d3dd66373a8d6
杨超 <qingchengmuyu@gmail.com>
2019-06-18 16:15:02 +0800

    省市区三级联动

--- meiduo_mall/meiduo_mall/apps/areas/urls.py
+++ meiduo_mall/meiduo_mall/apps/areas/urls.py
@@ -14,15 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('users.urls', namespace='users')),
-    url(r'^', include('contents.urls', namespace='content')),
-    url(r'^', include('verifications.urls', namespace='verifications')),
-    url(r'^', include('oauth.urls', namespace='oauth')),
-    url(r'^', include('areas.urls', namespace='areas')),
-
+    # url(r'^admin/', admin.site.urls),
+    url(r'^areas/$', views.AreasView.as_view()),
 
 ]
--- meiduo_mall/meiduo_mall/apps/areas/views.py
+++ meiduo_mall/meiduo_mall/apps/areas/views.py
@@ -1,3 +1,51 @@
+from django import http
+from django.core.cache import cache
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from .models import Area
+from meiduo_mall.utils.response_code import RETCODE
+
+
+class AreasView(View):
+    """省市区数据查询"""
+    def get(self, request):
+        area_id = request.GET.get('area_id')
+        if area_id is None:
+            province_list = cache.get('province_list')
+            if province_list is None:
+                province_qs = Area.objects.filter(parent=None)
+                province_list = []
+                for province_model in province_qs:
+                    province_list.append({
+                        'id': province_model.id,
+                        'name': province_model.name
+                    })
+                cache.set('province_list', province_list, 3600)
+            return http.JsonResponse({'code': RETCODE.OK, 'errmsg': "OK", 'province_list': province_list})
+        else:
+            sub_data = cache.get('sub_area_%s' % area_id)
+            if sub_data is None:
+                sub_qs = Area.objects.filter(parent_id=area_id)
+                try:
+                    parent_model = Area.objects.get(id=area_id)
+                except Area.DoesNotExist:
+                    return http.HttpResponseForbidden('area_id不存在')
+                sub_list = []
+                for sub_model in sub_qs:
+                    sub_list.append({
+                        'id': sub_model.id,
+                        'name': sub_model.name
+                    })
+                sub_data = {
+                    'id': parent_model.id,
+                    'name': parent_model.name,
+                    'subs': sub_list
+                }
+                cache.set('sub_area_%s' % area_id, sub_data, 3600)
+            return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'sub_data': sub_data})
+
+
+
+
+
