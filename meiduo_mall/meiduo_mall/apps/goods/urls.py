4d3ebe4d9269e4f464530cdf5977099dade1047a
杨超 <qingchengmuyu@gmail.com>
2019-06-23 17:15:10 +0800

    统计分类商品访问量

--- meiduo_mall/meiduo_mall/apps/goods/urls.py
+++ meiduo_mall/meiduo_mall/apps/goods/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     url(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/$', views.LastView.as_view()),
     url(r'^hot/(?P<category_id>\d+)/$', views.HotGoodsView.as_view()),
     url(r'^detail/(?P<sku_id>\d+)/$', views.DetailView.as_view()),
+    url(r'^visit/(?P<category_id>\d+)/$', views.DetailVisitView.as_view()),
 
 ]
